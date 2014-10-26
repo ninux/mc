38c0e0a8eb428d821edd057a7f95ebbd0dce1a62
Nino Ninux <nino.ninux@gmail.com>
2014-10-26 12:05:57 +0100

    prepared given exercise code for further development

---
+++ c/sw5/ex3/semaphore.c
@@ -0,0 +1,110 @@
+/*
+* semaphore.c - Semaphore
+*
+* Copyright (C) 2014 Ervin Mazlagic <nino.ninux@gmail.com>
+*
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or (at
+* your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* walong with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include "semaphore.h"
+
+/* !!! To do: Define a variable getPhaseTimeFunction of type
+   'pointer-to-function' for the callback function and set it to NULL */
+
+static int threadRunning = 0;
+static SemaphorStates_t state = RED;
+static pthread_t threadId;
+static pthread_attr_t threadAttr;
+
+/**
+ * Print current thread state
+ */
+static void printThreadState(int sleepTime)
+{
+	char stateStr[64];
+
+	switch (state) {
+	case GREEN:		strcpy(stateStr, "GREEN");		break;
+	case GREEN_BLINKING:	strcpy(stateStr, "GREEN BLINKING");	break;
+	case YELLOW:		strcpy(stateStr, "YELLOW");		break;
+	case RED:		strcpy(stateStr, "RED");		break;
+	default:		strcpy(stateStr, "UNDEFINED");		break;
+	}
+	printf("The semaphor is %-14s for %2d seconds\n", stateStr, sleepTime);
+	fflush(stdout);
+}
+
+static void* threadFunction(void* args)
+{
+	int sleepTime = 2;
+	printThreadState(sleepTime);
+	sleep(sleepTime);
+
+	while (threadRunning) {
+		switch (state) {
+		case GREEN:		state = GREEN_BLINKING;	break;
+		case GREEN_BLINKING:	state = YELLOW;		break;
+		case YELLOW:		state = RED;		break;
+		case RED:		state = GREEN;		break;
+		default: /* Should not happen, but added error recovery */
+					state = RED;		break;
+		}
+
+/* !!! To do: Call here the callback function to get the sleep time
+		if (getPhaseTimeFunction != NULL)
+		{
+			sleepTime = ...;
+*/
+		printThreadState(sleepTime);
+		sleep(sleepTime);
+	}
+	printf("Semaphor thread stopped\n");
+
+	return NULL;
+}
+
+
+/* !!! To do !!!
+void setSemaphorCallbackFunction(...)
+{
+	getPhaseTimerFunction = 
+}
+*/
+
+void startSemaphore()
+{
+	threadRunning = 1;
+
+	/* start thread */
+	pthread_attr_init(&threadAttr);
+	/* the third argument is a pointer to the thread function */
+	pthread_create(&threadId, &threadAttr, threadFunction, NULL);
+}
+
+void stopSemaphor()
+{
+	threadRunning = 0;
+
+	/* wait until thread is finished */
+	pthread_join(threadId, NULL);
+	pthread_attr_destroy(&threadAttr);
+}
---
+++ c/sw5/ex3/semaphore.h
@@ -0,0 +1,62 @@
+/*
+* semaphore.h - Semaphore
+*
+* Copyright (C) 2014 Ervin Mazlagic <nino.ninux@gmail.com>
+*
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or (at
+* your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* walong with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+#ifndef _SEMAPHORE_H
+#define _SEMAPHORE_H
+
+/**
+ * Implementation of a semaphor with a thread.
+ * Uses a callback function to define the various times.
+ */
+
+typedef enum SemaphoreStates_ {
+	GREEN = 1,
+	GREEN_BLINKING,
+	YELLOW,
+	RED
+} SemaphorStates_t;
+
+/**
+ * Set the callback function that defines the time of each phase.
+ * @param getPhaseTimePointer to function that returns the phase length in
+ * seconds.
+ *	The function gets as Parameter the current semaphore state.
+ */
+/* !!! To do !!! void setSemaphoreCallbackFunction(...); */
+
+/**
+ * Starts the semaphor.
+ * Within a thread, the semaphore changes its state from GREEN to GREEN_BLINKING
+ * to YELLOW to RED to GREEN to ...
+ * After each state change, the callbackfunction defined by
+ * setCallbackFunction(...) will be called to get the length of the
+ * corresponding phase.
+ */
+void startSemaphore();
+
+/**
+ * Stop the semaphor.
+ */
+void stopSemaphor();
+
+#endif /* _SEMAPHORE_H */
